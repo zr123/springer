d2881ee721b307b0014ee6fc2cb29b9579ea400f
zr123 <qizcvtukqwe123@gmail.com>
2017-01-31 09:24:45 +0100

    WIP

--- chess.c
+++ chess.c
@@ -7,7 +7,7 @@
 #endif
 
 const char* helptext = 
-"Verwendung: springer [options] <Startposition>\n\
+"Verwendung: springer [optionen] <Startposition>\n\
 Startposition zB. in der Form \"A6\" oder \"f1\" angeben \n\
 Options:\n\
   --help                Diese Hilfe anzeigen\n\
@@ -15,39 +15,31 @@ Options:\n\
   -h=<Wert>             Standardwert der Feldh\x94he \x84ndern\n\
   -w=<Wert>             Standardwert der Feldbreite \132ndern\n\
   -d                    Dynamische Ausgabe aktivieren (nur Windows)\n\
-  -csv=<Filename>       Feld nach Berechnung als CSV nach Filename schreiben\n\
+  -csv=<Filename>       Brett nach Berechnung als CSV nach Filename schreiben\n\
   -p=<n>                Ermittelt die n'te Permutation\n\
   ";
   
 int handleCommandLineArguments(parameters* param, int argc, char* argv[]){
-  initParameters(param);
-  loadParameterIni(param, "init.ini");
-  if(argc == 1)
+  if(param == NULL || argc == 1){
+    printf("Fehlende Eingabeparameter. Hilfe mit --help\n");
     return 1;
-  for(int i = 1; i < argc; ++i){
-    if(!strncmp(argv[i], "-", 1)){
-      parseArgument(param, argv[i]);
-    }else{
-      char posLetter;
-      int posNumber;
-      if(sscanf(argv[i], "%c%d", &posLetter, &posNumber) == 2){
-        param->startingPos_x = convertLetterToPosition(posLetter);
-        param->startingPos_y = posNumber;
-      }
-    }
   }
+  for(int i = 1; i < argc; ++i)
+    if(parseArgument(param, argv[i]))
+      return 1;
   return 0;
 }
 
-void initParameters(parameters* param){
-  strcpy(param->CSVfilename, "");
+parameters* initParameters(){
+  parameters* param = (parameters*)calloc(sizeof(parameters), 1);
+  if(param == NULL)
+    return NULL;
   param->boardWidth = 8;
   param->boardHeight = 8;
-  param->startingPos_x = -1;
-  param->startingPos_y = -1;
-  param->loop = 0;
-  param->dynamicOutput = 0;
-  param->permutation = 0;
+  //param->startingPos_x = -1;
+  //param->startingPos_y = -1;
+  loadParameterIni(param, "init.ini");
+  return param;
 }
 
 int loadParameterIni(parameters* param, const char* filename){
@@ -64,27 +56,39 @@ int loadParameterIni(parameters* param, const char* filename){
 }
 
 int parseArgument(parameters* param, const char* argument){
-  if(!strcmp(argument, "-help") || !strcmp(argument, "--help") || !strcmp(argument, "-h") || !strcmp(argument, "--h")){
-    printf("%s", helptext);
+  if(!strncmp(argument, "-", 1)){
+    if(!strcmp(argument, "-help") || !strcmp(argument, "--help") || !strcmp(argument, "-h") || !strcmp(argument, "--h")){
+      printf("%s", helptext);
+      return 1;
+    }
+    if(!strcmp(argument, "-loop") || !strcmp(argument, "-l")){
+      param->loop = 1;
+      return 0;
+    }
+    if(!strcmp(argument, "-dynamic") || !strcmp(argument, "-d")){
+      param->dynamicOutput = 1;
+      return 0;
+    }
+    if(sscanf(argument, "-w=%d", &param->boardWidth) || sscanf(argument, "-width=%d", &param->boardWidth))
+      return 0;
+    if(sscanf(argument, "-h=%d", &param->boardHeight) || sscanf(argument, "-height=%d", &param->boardHeight))
+      return 0;
+    if(sscanf(argument, "-csv=%s", param->CSVfilename))
+      return 0;
+    if(sscanf(argument, "-p=%d", &param->permutation))
+      return 0;
+    printf("Ung\x81ltige Option: %s\n", argument);
     return 1;
+  }else{
+    char posLetter;
+    if(sscanf(argument, "%c%d", &posLetter, &param->startingPos_y) == 2){
+      param->startingPos_x = convertLetterToPosition(posLetter);
+      return 0;
+    }else{
+      printf("Ung\x81ltige Eingabe: %s\n", argument);
+      return 1;
+    }
   }
-  if(!strcmp(argument, "-loop") || !strcmp(argument, "-l")){
-    param->loop = 1;
-    return 0;
-  }
-  if(!strcmp(argument, "-dynamic") || !strcmp(argument, "-d")){
-    param->dynamicOutput = 1;
-    return 0;
-  }
-  if(sscanf(argument, "-w=%d", &param->boardWidth) == 1 || sscanf(argument, "-width=%d", &param->boardWidth) == 1)
-    return 0;
-  if(sscanf(argument, "-h=%d", &param->boardHeight) == 1 || sscanf(argument, "-height=%d", &param->boardHeight) == 1)
-    return 0;
-  if(sscanf(argument, "-csv=%s", param->CSVfilename))
-    return 0;
-  if(sscanf(argument, "-p=%d", &param->permutation) == 1)
-    return 0;
-  return 1;
 }
 
 // den Buchstabenteil aus "A6" oder "f1" zu dem integer-Positionswert konvertieren
@@ -94,88 +98,91 @@ int convertLetterToPosition(char letter){
   return pos_x;
 }
 
-// Prüfung ob das Feld in einem Zustand ist in dem keine Lösung mehr möglich ist
-int checkDeadEnd(board* boardPointer){
-  /*
+// Prüfung ob ein Feld isoliert (nicht mehr anspringbar) ist
+int checkFieldIsolation(board* boardPointer){
   for(int i = 0; i < boardPointer->width * boardPointer->height; ++i)
     if(boardPointer->fields[i].value == 0 && boardPointer->fields[i].remainingAccessibleFieldCount == 0)
-      return 2;
+      return 1;
   return 0;
-  */
+}
+
+// Es dürfen maximal 2 Felder nur noch eine Sprungoption haben (derzeitiges Feld und schließliches Endfeld) 
+int checkDeadEnd(board* boardPointer){
+  // eigentlich dürfen nur currentfield und endingfield remainingAccessibleFieldCount==1 haben
   int count = 0;
   for(int i = 0; i < boardPointer->width * boardPointer->height; ++i)
     if(boardPointer->fields[i].value == 0 && boardPointer->fields[i].remainingAccessibleFieldCount == 1)
       count++;
-  return count;
-  
+  return (count>2);  
 }
 
+// Feldpointer aus Koordinaten ermitteln
 field* getFieldPointer(board* boardPointer, int x, int y){
   return &boardPointer->fields[boardPointer->width*y + x];  
 }
 
+// Sprungmarkierung setzten und Anzahl der Anspringbaren Felder reduzieren
 void setFieldValue(field* fieldPointer, int value){
   fieldPointer->value = value;
-  for(int i = 0; i < fieldPointer->accessibleFieldCount; ++i){
-    field* nextField = fieldPointer->accessibleFields[i];
-    if(nextField != NULL)
-      nextField->remainingAccessibleFieldCount -= 1;
-  }
+  adjustNextAccessibleFieldsCount(fieldPointer, -1);
 }
 
+// Gegenfunktion zu setFieldValue um ein Feld zurückzusetzen
 void resetFieldValue(field* fieldPointer){
   fieldPointer->value = 0;
-  for(int i = 0; i < fieldPointer->accessibleFieldCount; ++i){
-    field* nextField = fieldPointer->accessibleFields[i];
-    if(nextField != NULL)
-      nextField->remainingAccessibleFieldCount += 1;
-  }
+  adjustNextAccessibleFieldsCount(fieldPointer, +1);
 }
 
+// Für alle anspringbaren Felder die Anzahl der Sprungmöglichkeiten anpassen, wenn das Feld angesprungen/zurückgesetzt wird
+void adjustNextAccessibleFieldsCount(field* fieldPointer, int adjustmentAmount){
+  for(int i = 0; i < fieldPointer->accessibleFieldCount; ++i)
+    if(fieldPointer->accessibleFields[i] != NULL)
+      fieldPointer->accessibleFields[i]->remainingAccessibleFieldCount += adjustmentAmount;
+}
+
+// Derzeitiges Brett in eine CSV (comma seperated values) schreiben
 int printBoardToCSVFile(board* boardPointer, const char* filename){
-  if(filename == NULL || strlen(filename) == 0)return 0;
+  if(boardPointer == NULL)
+    return 1;
   FILE* handle = fopen(filename, "w");
-  if(handle == NULL)return 1;
-  for(int y = boardPointer->height-1; y >= 0; --y){
-    fprintf(handle, "%d", getFieldPointer(boardPointer, 0, y)->value); 
-    for(int x = 1; x < boardPointer->width; ++x){
-      fprintf(handle, ";%d", getFieldPointer(boardPointer, x, y)->value); 
-    }
-    fprintf(handle, "\n");
+  if(handle == NULL)
+    return 1;
+  for(int i = 0; i < boardPointer->height*boardPointer->width; ++i){
+    fprintf(handle, "%d,", boardPointer->fields[i].value);
+    if(i % boardPointer->width == 0)
+      fprintf(handle, "\n");
   }
   fclose(handle);
   return 0;
 }
 
-int checkBounds(board* boardPointer, int pos_x, int pos_y){
-  if(pos_x < 0 || pos_y < 0)return 0;
-  if(pos_x >= boardPointer->width || pos_y >= boardPointer->height)return 0;
+// Prüfung ob die Koordinate für das derzeitige Brett erlaubt ist
+int checkBounds(board* boardPointer, coord position){
+  if(position.x < 0 || position.y < 0)return 0;
+  if(position.x >= boardPointer->width || position.y >= boardPointer->height)return 0;
   return 1;
 }
 
-field* knightMove(board* boardPointer, int pos_x, int pos_y, int direction){
-  // Bitte einen Moment inne halten und bewundern wie elegant ich das hier gelöst habe.
-  // from 0 to 7 clockwise, starting with "upRight" (2 Up, 1 Right)
-  if(direction < 0 || direction > 7)return NULL; 
+// Ermittelt die 8 möglichen anspringbaren Felder, ausgehend vom derzeitigen Feld ...
+// ... für 'direction' von 0 bis 7 im Uhrzeigersinn, beginnend oben rechts. 
+// Dazu werden die 3 kleinsten Bits von 'direction' ausgewertet
+coord knightMove(coord position, int direction){
   int x = 1, y = 2;
   if((direction+1) & 2)x = 2, y = 1;
   if((direction+2) & 4)y *= -1;
   if(direction & 4)x *= -1;
-  pos_x += x;
-  pos_y += y;
-  if(checkBounds(boardPointer, pos_x, pos_y))
-    return getFieldPointer(boardPointer, pos_x, pos_y);
-  else
-    return NULL;
+  position.x += x;
+  position.y += y;
+  return position;
 }
 
 void linkFields(board* boardPointer, int x, int y){
   field* fieldPointer = getFieldPointer(boardPointer, x, y);
   int count = 0;
   for(int direction = 0; direction < 8; ++direction){
-    field* nextField = knightMove(boardPointer, x, y, direction);
-    if(nextField != NULL){
-      fieldPointer->accessibleFields[count] = nextField;
+    coord coordinate = knightMove((coord){x, y}, direction);
+    if(checkBounds(boardPointer, coordinate)){
+      fieldPointer->accessibleFields[count] = getFieldPointer(boardPointer, coordinate.x, coordinate.y);;
       count++;
     }
   }
@@ -185,11 +192,7 @@ void linkFields(board* boardPointer, int x, int y){
 
 void initField(board* boardPointer, int pos_x, int pos_y){
   field* fieldPointer = getFieldPointer(boardPointer, pos_x, pos_y);
-  fieldPointer->value = 0;
-  fieldPointer->x = pos_x;
-  fieldPointer->y = pos_y;
-  for(int i = 0; i < 8; ++i)
-    fieldPointer->accessibleFields[i] = NULL;
+  fieldPointer->outputFieldLocation = boardPointer->outputString + boardPointer->outputWidth*(2*pos_y +2) + (boardPointer->fieldSize+1)*pos_x + 1;
   linkFields(boardPointer, pos_x, pos_y);
 }
 
@@ -203,28 +206,31 @@ int initBoard(board* boardPointer, parameters* param){
   boardPointer->endingField = NULL;
   boardPointer->dynamicOutput = param->dynamicOutput;
   boardPointer->permutationCount = param->permutation;
+  // schön machen start
+  if(param->loop)
+    boardPointer->endingField = getFieldPointer(boardPointer, param->startingPos_x, boardPointer->height - param->startingPos_y);
+  // schön machen end
   if(boardPointer->dynamicOutput){
     #if defined(_WIN32) || defined(__CYGWIN__)
     boardPointer->consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
     GetConsoleScreenBufferInfo(boardPointer->consoleHandle, &boardPointer->bufferInfo);
     #endif
   }
+  if(initOutputField(boardPointer))
+    return 1;
+  drawOutputField(boardPointer);
   // link all fields accessible by a knights move to each other
   for(int x = 0; x < boardPointer->width; ++x)
     for(int y = 0; y < boardPointer->height; ++y)
       initField(boardPointer, x, y);
-  if(initOutputField(boardPointer))
-    return 1;
-  drawOutputField(boardPointer);
   return 0;
 }
 
 int countDigits(int number){
-  if(number == 0){
+  if(number == 0)
     return 0;
-  }else{
+  else
     return 1 + countDigits(number/10);
-  }
 }
 
 void drawLine(board* boardPointer, int lineNumber, char leftDelimiter, char contentPlaceholder, char middleDelimiter, char rightDelimiter){
@@ -246,7 +252,7 @@ void drawOutputField(board* boardPointer){
   drawLine(boardPointer, boardPointer->outputHeight-1, '\xC8', '\xCD', '\xCA', '\xBC');
   char letter = 'a';
   for(int x = 0; x < boardPointer->width; ++x){
-    setOutputFieldByStr(boardPointer, x, 0, letter);
+    setOutputFieldByStr(boardPointer, x, 0, 'a'+x);
     letter++;
   }
   for(int y = boardPointer->height; y > 0; --y)
@@ -270,6 +276,15 @@ int initOutputField(board* boardPointer){
   return 0;
 }
 
+void updateOutputField(board* boardPointer, field* fieldPointer){
+  char buffer[boardPointer->fieldSize+1];
+  if(fieldPointer->value != 0)
+    sprintf(buffer, "%*d", boardPointer->fieldSize, fieldPointer->value);
+  else
+    sprintf(buffer, "%*c", boardPointer->fieldSize, ' ');
+  strncpy(fieldPointer->outputFieldLocation, buffer, boardPointer->fieldSize);
+}
+
 void setOutputFieldByValue(board* boardPointer, int x, int y, int value){
   char buffer[boardPointer->fieldSize+1];
   sprintf(buffer, "%*d", boardPointer->fieldSize, value);
@@ -284,21 +299,18 @@ void setOutputFieldByStr(board* boardPointer, int x, int y, const char chr){
   strncpy(location, buffer, boardPointer->fieldSize);
 }
 
-// Vergleichsfunktion. Wird zur Sortierung des accessibleFieldCount-Arrays benötigt
+// Vergleichsfunktion. Wird zur Sortierung nach restlich anspringbaren Felder des accessibleFields-Arrays benötigt
 int compareFieldValues(const void* a, const void* b){
   field* field1 = *(field**)a;
   field* field2 = *(field**)b;
   if(field1->remainingAccessibleFieldCount < field2->remainingAccessibleFieldCount)return -1;
   if(field1->remainingAccessibleFieldCount > field2->remainingAccessibleFieldCount)return 1;
-  if(field1->remainingAccessibleFieldCount == field2->remainingAccessibleFieldCount)return 0;
+  return 0;
 }
 
 void updateOutputString(board* boardPointer){
-  for(int y = 0; y < boardPointer->height; ++y)
-    for(int x = 0; x < boardPointer->width; ++x){
-      field* fp = getFieldPointer(boardPointer, x, y);
-      setOutputFieldByValue(boardPointer, x, y+1, fp->value);
-    }
+  for(int i = 0; i < boardPointer->height*boardPointer->width; ++i)
+    updateOutputField(boardPointer, &boardPointer->fields[i]);
 }
 
 int solve(board* boardPointer, field* currentField, int count){
@@ -306,44 +318,42 @@ int solve(board* boardPointer, field* currentField, int count){
   if(boardPointer->endingField != NULL && boardPointer->endingField->remainingAccessibleFieldCount == 0)
     return 0;
   // recursion "good end"
-  if(count == boardPointer->height*boardPointer->width){
+  if(count == boardPointer->height*boardPointer->width)
     if(boardPointer->permutationCount > 0){
       boardPointer->permutationCount--;
       return 0;
     }else{
+      setFieldValue(currentField, count);
       if(boardPointer->dynamicOutput){
-        setOutputFieldByValue(boardPointer, currentField->x, currentField->y+1, count);
+        updateOutputField(boardPointer, currentField);
         SetConsoleCursorPosition(boardPointer->consoleHandle, boardPointer->bufferInfo.dwCursorPosition);
         printf("%s", boardPointer->outputString);
       }
-      setFieldValue(currentField, count);
       return 1;
     }
-  }
   //check if any field has been set to 0, or multiple have been set to 1
-  if(checkDeadEnd(boardPointer) > 2){
+  if(checkFieldIsolation(boardPointer) || checkDeadEnd(boardPointer))
     return 0;
-  }
   // set field and next recursion level 
   setFieldValue(currentField, count);
   
   if(boardPointer->dynamicOutput){
-    setOutputFieldByValue(boardPointer, currentField->x, currentField->y+1, count);
+    updateOutputField(boardPointer, currentField);
     SetConsoleCursorPosition(boardPointer->consoleHandle, boardPointer->bufferInfo.dwCursorPosition);
     printf("%s", boardPointer->outputString);
   }
   qsort(currentField->accessibleFields, currentField->accessibleFieldCount, sizeof(field*), compareFieldValues);
-  for(int direction = 0; direction < currentField->accessibleFieldCount; ++direction){
+  for(int direction = 0; direction < currentField->accessibleFieldCount; ++direction)
     if(currentField->accessibleFields[direction]->value == 0){
       if(solve(boardPointer, currentField->accessibleFields[direction], count+1))
         return 1;
-      if(currentField->accessibleFields[direction]->value == 2)
-        break;
+      // unnötig
+      //if(currentField->accessibleFields[direction]->value == 2)
+      //  break;
     }
-  }
   // recursion bad end -> reset field
   resetFieldValue(currentField);
   if(boardPointer->dynamicOutput)
-    setOutputFieldByStr(boardPointer, currentField->x, currentField->y+1, ' ');
+    updateOutputField(boardPointer, currentField);
   return 0;
 }
