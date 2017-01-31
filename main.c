3265a8815c5e502c4ecee5375dabe19062514444
zr123 <qizcvtukqwe123@gmail.com>
2017-01-31 09:26:16 +0100

    WIP

--- main.c
+++ main.c
@@ -10,36 +10,30 @@
   - Saubere Fehlerbehandlung und Input-Prüfung
   - Readme Schreiben
   - Windows/Linux Problematik
-  - braucht field wirklich sein x und y?
-  - -s|seperator
+  - -e|end=
 */
 int main(int argc, char* argv[]){
-  parameters p;
-  board b;
-
-  if(handleCommandLineArguments(&p, argc, argv)){
-    printf("Fehlende Eingabeparameter. Hilfe mit --help\n");
+  parameters* p = initParameters();
+  if(handleCommandLineArguments(p, argc, argv))
     return 1;
-  }
-  if(initBoard(&b, &p)){
+  board b;
+  if(initBoard(&b, p)){
     printf("Initialisierungsfehler. Programmausführung abgebrochen.");
     return 1;
   }
-  // Buggy, fix => analysiert position noch nicht korrekt
-  //if(!checkBounds(&b, p.startingPos_x, p.startingPos_y))
-  //  return 1;
-  
-  int startingClock = clock();
-  if(p.startingPos_x >= 0 && p.startingPos_y >= 0){
-    if(p.loop)
-      b.endingField = getFieldPointer(&b, p.startingPos_x, b.height - p.startingPos_y);
-    solve(&b, getFieldPointer(&b, p.startingPos_x, b.height - p.startingPos_y), 1);
-    if(strlen(p.CSVfilename) > 0)
-      printBoardToCSVFile(&b, p.CSVfilename); 
-    updateOutputString(&b);
-    if(!p.dynamicOutput)
-      printf(b.outputString);
-    printf("\nDone in %d ms.\n", clock()-startingClock);
+  if(!checkBounds(&b, (coord){p->startingPos_x, p->startingPos_y-1})){
+    printf("Ung\x81ltige Anfangsposition.\n", p->startingPos_x, p->startingPos_y);
+    return 1;
   }
+  
+  int timer = clock();
+  solve(&b, getFieldPointer(&b, p->startingPos_x, b.height - p->startingPos_y), 1);
+  timer = clock()-timer;
+  if(strlen(p->CSVfilename) > 0)
+    printBoardToCSVFile(&b, p->CSVfilename); 
+  updateOutputString(&b);
+  if(!p->dynamicOutput)
+    printf(b.outputString);
+  printf("\nDone in %d ms.\n", timer);
   return 0;
 }
