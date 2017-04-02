4fb2897f96ae54541f89bfa30a649ec942f02e74
zr123 <qizcvtukqwe123@gmail.com>
2017-04-02 18:45:02 +0200

    Final

--- main.c
+++ main.c
@@ -1,46 +1,96 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
 #include "chess.h"
+#include "languageFile.c"
 
-/* TODO
-  - Sauberes Refactoring
-  - Kommentieren
-  - header ordentlich ordnen
-  - Saubere Fehlerbehandlung und Input-Prüfung
-  - Readme Schreiben
-  - Windows/Linux Problematik
-  - -e|end=
+/*
+  Kleinigkeiten die noch verbessert werden könnten:
+    Problem 1:
+      - Parameter und Kommandozeilenargumente werden Hart gecoded ausgewertet.
+      - Schön wäre eine dynamischere Auswertung der .ini und Argumente.
+    Lösung:
+      - Eigenens Struct für die Parameter, mit name, typ (flag oder wert) und Synonymliste.
+        -> Das Handling von Parametern und Kommandozeilenargumente modularisieren.
+    Problem 2:
+      - Tests fehlen leider vollständig.
+    Lösung:
+      - Tests schreiben. Ein großtteil der Funktionen ist in einen gut Unit-testbaren Zustand.
+    Problem 3:
+      - Anpassungen für *nix Systeme fehlen.
+    Lösung:
+      - Machen.
+    
+    Features die noch gemacht werden könnten:
+      -e|end=xx Argument um das Endfeld direkt zu setzten
+      -ein Argument/Parameter um die Konsolenausgabe komplett abzuschalten
+      -ein Teile-und-Herrsche Algorythmus auf Basis des bestehenden für SEHR große Felder
 */
+
+int prepareParameters();
+
 int main(int argc, char* argv[]){
-  // WIP: parameter struct kann eigentlich weg
+  // parameter für die funktion ermitteln
   parameters* p = initParameters();
-  if(loadParameterIni(p, "parameter.ini"))
-    printf("Warnung: parameter.ini konnte nicht korrekt geladen werden.\n");
-  // WIP
-  if(handleCommandLineArguments(p, argc, argv))
-    return 1;
-  //p->startingPos = getRandomCoord(p->boardWidth, p->boardHeight);
-  board* b = initBoard(p);
-  if(b == NULL){
-    printf("Initialisierungsfehler. Programmausführung abgebrochen.");
+  if(loadParameterIni(p)){
+    printf(strLoadParameterIniFailed);
+    if(createDefaultParameterIni())
+      printf("%s", strDefaultParameterIniCreated);
+    else
+      printf("%s", strDefaultParameterIniCreationFailed);
+  } 
+  if(argc == 1 && !p->autoRandom){
+    printf("%s", strMissingArguments);
     return 1;
   }
+  int e = handleCommandLineArguments(p, argc, argv);
+  if(e > 0){
+    printf(strInvalidArgument, argv[e]);
+    return 1;
+  }else{
+    normalizeCoordinate(&p->startingPos, p->boardHeight);
+  }
+  if(p->helpFlag){
+    printf("%s", helptext);
+    return 0;
+  }
+  if(p->autoRandom && p->startingPos.x == 0 && p->startingPos.y == p->boardHeight)
+    p->startingPos = getRandomCoord(p->boardWidth, p->boardHeight);
+  
+  // Board initieren 
+  board* b = initBoard(p->boardWidth, p->boardHeight);
+  if(b == NULL){
+    printf("%s", strInitializationFailed);
+    return 1;
+  }
+  determineEndingField(b, p);
+  b->dynamicOutputDelay = p->dynamicOutputDelay;
+  b->permutationCount = p->permutation;
   if(!checkBounds(b, p->startingPos)){
-    printf("Ung\x81ltige Anfangsposition.\n");
+    printf("%s", strInvalidStartingPosition);
     return 1;
   }
   
-  if(p->dynamicOutput)
-    printf(b->outputString);
+  // 
+  if(p->dynamicOutputDelay)
+    printf("%s", b->outputString);
   int timer = clock();
-  solve(b, getFieldPointer(b, p->startingPos), 1);
+  bool success = solve(b, getFieldPointer(b, p->startingPos), 1);
   timer = clock()-timer;
   if(strlen(p->CSVfilename) > 0)
     printBoardToCSVFile(b, p->CSVfilename); 
+  
+  // Ergebnis ausgeben
+  #if defined(_WIN32) || defined(__CYGWIN__)
+  if(p->dynamicOutputDelay) 
+    SetConsoleCursorPosition(b->consoleHandle, b->bufferInfo.dwCursorPosition);
+  #endif
   updateOutputString(b);
-  if(!p->dynamicOutput)
-    printf(b->outputString);
-  printf("\nDone in %d ms.\n", timer);
+  printf("%s", b->outputString);
+  printf(strTimerDone, timer);
+  if(success == false)
+    printf("%s", strNoPathForInput);
+  
   return 0;
 }
